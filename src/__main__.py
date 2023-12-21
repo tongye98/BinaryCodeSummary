04bc7270bd884e250661f86901e746cffeb304a2
tongye98 <791161009@qq.com>
2023-12-21 15:14:15 +0800

    small

--- src/__main__.py
+++ src/__main__.py
@@ -6,7 +6,7 @@ from src.test import test
 def main():
     parser = argparse.ArgumentParser("BinaryCodeSummary")
     parser.add_argument("mode", type=str, default="train", choices=["train", "test", "unittest"])
-    parser.add_argument("--config", type=str, default="configs/dataset_gcc-7.3.0_arm_32_O3_strip/O1_test3_cszx.yaml")
+    parser.add_argument("--config", type=str, default="configs/dataset_gcc-7.3.0_arm_32_O1_strip/O1_test3_cszx.yaml")
     parser.add_argument("--gpu", type=str, default='1')
     parser.add_argument("--ckpt", type=str, default=None, help="Model Checkpoint")
     args = parser.parse_args()
